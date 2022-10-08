9ef3e9f3118603a0d6e8f2ba0ce9f9419059e75e
Oleg Romanov <mailtome@ua.fm>
2022-10-08 13:51:26 +0300

    Initial commit

---
+++ master.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# initialize variables
+DBNAME=TESTBASE
+MASTERHOST=192.168.56.11
+DBPASSWD=tustan
+
+# replication user
+RUSER=replicator
+RPASSWD=tustan
+
+# copy mysql config
+cp /vagrant/50-master.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
+
+sudo service mysql restart
+
+# create replication user and grant access
+mysql -uroot -p$DBPASSWD -e "CREATE USER '$RUSER'@'$DBHOST' IDENTIFIED BY '$RPASSWD';"
+mysql -uroot -p$DBPASSWD -e "GRANT REPLICATION SLAVE ON *.* TO '$RUSER'@'$DBHOST';"
+mysql -uroot -p$DBPASSWD -e "FLUSH PRIVILEGES;"
+
+sudo service mysql restart
+
+
+
